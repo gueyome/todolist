3b19d18532cd97109399bf24dd226c103e309e27
guillaume <gueyome@hotmail.fr>
2019-11-21 11:39:45 +0100

    commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,13 +8,24 @@ before_action :authenticate_user!
     @task = Task.new(task_params)
     @category = Category.find(category_params)
     @task.category = @category
+    puts "************test************"
     if @task.save
-      redirect_to root_path
-      flash[:notice] = "Task created"
+      respond_to do |format|
+        format.html {
+          puts "************test HTML************"
+          redirect_to root_path
+          flash[:notice] = "Task created"
+        }
+        format.js { 
+          puts "************test JS************"
+          puts "ok c'est bien au format JS" 
+        }
+      end
     else
       redirect_to root_path
       flash[:notice] = "Please try again"
     end
+    
   end
 
   def edit
@@ -25,9 +36,25 @@ before_action :authenticate_user!
 
   def update
     @task = Task.find(params[:id])
-    @task.update(task_params)
-    redirect_to tasks_path
-    flash[:notice] = "Task edited"
+    puts "*******direction ok vers put*****"
+    puts params
+    respond_to do |format|
+      format.html { 
+        puts "edition html"
+        @task.update(task_params)
+        redirect_to tasks_path
+        flash[:notice] = "Task edited" }
+      format.js { 
+        puts "edition js"
+        if @task.status == false
+          @task.status = true
+        else 
+          @task.status = true
+        end
+        @task.save 
+      }
+    end
+    
   end
 
   def index
@@ -37,7 +64,10 @@ before_action :authenticate_user!
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
-    redirect_to root_path
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
   end
 
 
