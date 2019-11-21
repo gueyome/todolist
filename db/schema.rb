601601c94eba6e41b73f8f8df7903e2800735979
jeremyFRANCOIS <jeremy.francois.esg@gmail.com>
2019-11-21 17:16:49 +0100

    boolean read done for emails table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_105017) do
+ActiveRecord::Schema.define(version: 2019_11_21_160953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_11_21_105017) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "read", default: false
   end
 
   create_table "tasks", force: :cascade do |t|
