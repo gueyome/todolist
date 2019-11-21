da5b299849079956cb07bc996e08dba8f4760bb2
jeremyFRANCOIS <jeremy.francois.esg@gmail.com>
2019-11-21 18:13:37 +0100

    trying to make show and update work

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -21,7 +21,30 @@ class EmailsController < ApplicationController
       format.html { redirect_to email_path }
       format.js {
         puts "******OK JS****"
-       }
+      }
+    end
+  end
+
+  def update
+    @email = Email.find(params[:id])
+    puts params
+    if params[:origin] == "object"
+      puts "#"*60
+      puts "origin object"
+      puts "#"*60
+      @email.update(read: true)
+    elsif params[:origin] == "btn"
+      if @email.read == false
+      puts "#"*60
+      puts "origin btn false"
+      puts "#"*60
+        @email.update(read: true)
+      else
+      puts "#"*60
+      puts "origin btn true"
+      puts "#"*60
+        @email.update(read: false)
+      end
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+get 'home', to: 'home#index'
   root to: "emails#index"
   resources :tasks, except: [:show]
   resources :emails
